ee2d7ddd4d6f5004838487a02ee60ee28597feee
Charlie Jiang <yixjiang@deloitte.com.au>
2017-06-08 17:07:20 +1000

    Not working - flash camera

--- app/src/main/java/com/charliejiang/morsetorch/MainActivity.kt
+++ app/src/main/java/com/charliejiang/morsetorch/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.charliejiang.morsetorch
 
+import android.content.Context
+import android.hardware.camera2.CameraManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val context = this@MainActivity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById(R.id.flashlightToggleButton).setOnClickListener {
+            val cameraManage =  context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            cameraManage.cameraIdList.forEach { cameraId-> cameraManage.setTorchMode(cameraId, true); }
+
+        }
     }
+
+
 }
