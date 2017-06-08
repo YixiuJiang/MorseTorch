e7287d062a3b48e1aca55ec1484b86126a4c07b4
Yixiu Jiang <yixiu.jiang@gmail.com>
2017-06-08 23:19:30 +1000

    add flash light check

--- testtorch/src/main/java/com/example/testtorch/MainActivity.kt
+++ testtorch/src/main/java/com/example/testtorch/MainActivity.kt
@@ -2,17 +2,29 @@ package com.example.testtorch
 
 import android.app.Activity
 import android.content.Context
+import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.widget.Toast
+
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById(R.id.flashlightToggleButton).setOnClickListener {
-            cameraManager.cameraIdList.forEach { cameraId -> cameraManager.setTorchMode(cameraId, true); }
+            cameraManager.cameraIdList.forEach { cameraId ->
+                val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
+                val flashAvailable = cameraCharacteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)
+                if (flashAvailable)
+                    cameraManager.setTorchMode(cameraId, true)
+                else
+                    Toast.makeText(this@MainActivity, "Flash not available", Toast.LENGTH_SHORT).show()
+
+            }
         }
     }
 }
